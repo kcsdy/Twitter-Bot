ca4af12680ebe4380db3b726a13a761aed58d3a8
kcsdy <chakrabortykuntal27@gmail.com>
2025-01-30 03:31:18 +0000

    Twitter bot now posts a automated tweet and like top five posts of a given hastag

--- tests/twitterBot.spec.js
+++ tests/twitterBot.spec.js
@@ -20,13 +20,35 @@ test('Twitter Login Bot', async ({ page }) => {
   
   await page.locator("a[href='/compose/post']").click();
   await expect(page).toHaveURL('https://x.com/compose/post');
+  await page.waitForTimeout(2000);
 
   //Write a tweet
-  const Tweet_text= "A new start";
+  const Tweet_text= "Testing bot 01fffasdsada";
   await page.locator("(//div[@aria-label='Post text'])[1]").fill(Tweet_text);
 
   //Post the tweet
   await page.locator("button[data-testid='tweetButton']").click();
+  await page.waitForTimeout(2000);
   await page.locator("button[data-testid='app-bar-close']").click();
+
+  //Search of a tweet using hastag
+  await page.locator("a[href='/explore']").click();
+  await expect(page).toHaveURL('https://x.com/explore');
+  await page.getByPlaceholder("Search").fill("#counter-strike");
+  await page.keyboard.press("Enter");
+  await page.locator("//span[text()='Latest']").click();
+  await page.waitForTimeout(5000);
+
+  // Like the first 5 tweets
+  const likeButtons = await page.$$("button[data-testid='like']");
+
+  for(let i=0;i<Math.min(likeButtons.length, 5);i++)
+  {
+    await likeButtons[i].click();
+    console.log(`Liked Tweet ${i+1}`);
+    await page.waitForTimeout(2000);
+  }
+  console.log("Liking complete!");
+
   
 });
