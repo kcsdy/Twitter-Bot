83e02b63f8958a99b894e8d74eb22b254d7e424c
kcsdy <chakrabortykuntal27@gmail.com>
2025-02-02 16:06:26 +0000

    changed the run command in yml file

--- playwright.config.js
+++ playwright.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     trace: 'retain-on-failure',
   },
 
